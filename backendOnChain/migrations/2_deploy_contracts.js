262dfa5da4afa9da3d7fdbc58b46115eb01e5ca8
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-12-18 13:27:06 -0300

    Migration das redes ganache/development com a 7º conta do ganache

--- backendOnChain/migrations/2_deploy_contracts.js
+++ backendOnChain/migrations/2_deploy_contracts.js
@@ -1,5 +1,21 @@
 const SBToken = artifacts.require("BetToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(SBToken).then();
+module.exports = async function (deployer, network, accounts) {
+  if (network === "ganache" || network === "development") {
+    // Creation contract transaction is gonna be sent by 7º Ganache account
+    const owner = accounts[6];
+    const web3Accounts = await web3.eth.getAccounts();
+    console.log(`web3 accounts: `, web3Accounts);
+    deployer.deploy(SBToken, {from: owner}); //.then();
+  } else if (network == "ropsten") {
+    //console.log(await web3.eth.getAccounts());
+    //TODO: implementar migração para rede ropsten
+    throw new Error(
+      `Migration to network ${network} still pendent of implementation`
+    );
+  } else {
+    throw new Error(
+      `Migration to network ${network} still pendent of implementation`
+    );
+  }
 };
