acaf4baac2eca8092f727950b643f4c93abf5b58
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-01-16 12:44:18 -0300

    Criação do atributo "result" para poder computar os prêmios. A ideia anterior era iterar pelas Bets e incluir as vencedoras em um array "winners". Entretanto, como Solidity não permite array de memória de tamanho dinâmico, essa solução pareceu mais viável

--- backendOnChain/contracts/structs/Bet.sol
+++ backendOnChain/contracts/structs/Bet.sol
@@ -7,4 +7,8 @@ struct Bet {
     address bettor;
     Score score;
     uint256 value;
+    // if zero, result not defined yet.
+    // if one, the bet did not match he final score
+    // if two, the bet match the final score and WON
+    uint8 result;
 }
