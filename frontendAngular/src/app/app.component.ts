b8707512bbb07b1aceb110da16aa08141af59a1c
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-01-27 20:33:51 -0300

    Console log dos endereços dos contratos definidos nos environments

--- frontendAngular/src/app/app.component.ts
+++ frontendAngular/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from 'src/environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Soccer Bet Ðapp';
   icon = 'sports_soccer';
+
+  ngOnInit(): void {
+    console.log(`BetToken.address`, environment.betTokenAddress);
+    console.log(`GameFactory.address`, environment.gameFactoryAddress);
+  }
 }
