20df2d7bb49b2d0400b03fe2036cab813b745c34
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-02-03 18:37:04 -0300

    Removendo referência ao arquivo '.secret' que seria para guardar o mneumônico da ropsten

--- backendOnChain/truffle-config.js
+++ backendOnChain/truffle-config.js
@@ -21,7 +21,7 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 //
 const fs = require("fs");
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = ""; //fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
