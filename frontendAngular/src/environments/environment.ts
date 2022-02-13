ffe553d941b08baef4e76f9cc900997ea3bf2288
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-02-13 12:41:32 -0300

    Atualização endereços contratos

--- frontendAngular/src/environments/environment.ts
+++ frontendAngular/src/environments/environment.ts
@@ -8,9 +8,9 @@ export const environment = {
   //betTokenAddress: '0xAec06a3112Ce416701a8C0333E502376205073fc',
   //gameFactoryAddress: '0x8F7364cF8A2383e25818552692B7399712eeb8EF',
   //notebook Vaio - Ganache v7
-  betTokenAddress: '0x85e855b22F01BdD33eE194490c7eB16b7EdaC019',
-  gameFactoryAddress: '0x06D0020790D42df662bB04Ab01f0208Cfbbb956F',
-  CalculatorAddress: '0x61A73d058D16D8d9cAabcfc9Da6a942Ef1C57C4A',
+  betTokenAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  CalculatorAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+  gameFactoryAddress: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
   //GANACHE 2.5.4
   // betTokenAddress: '0x85e855b22F01BdD33eE194490c7eB16b7EdaC019',
   // gameFactoryAddress: '0x06D0020790D42df662bB04Ab01f0208Cfbbb956F',
