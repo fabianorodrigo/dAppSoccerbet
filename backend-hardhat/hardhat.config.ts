ea2197f3f214fe044ba15df4e866049e620ad11c
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-02-27 17:23:07 -0300

    Configuração Solidity 0.8.11

--- backend-hardhat/hardhat.config.ts
+++ backend-hardhat/hardhat.config.ts
@@ -19,5 +19,5 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
-  solidity: "0.8.4",
+  solidity: "0.8.11",
 };
