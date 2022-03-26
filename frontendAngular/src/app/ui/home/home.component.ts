4613d0e07b3fb426bde1ac1063dc6f01cd53e01c
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-03-26 16:37:40 -0300

    Exibição dos endereços e owners dos contratos na Home

--- frontendAngular/src/app/ui/home/home.component.ts
+++ frontendAngular/src/app/ui/home/home.component.ts
@@ -1,4 +1,6 @@
+import { environment } from './../../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { BetTokenService, GameFactoryService } from 'src/app/contracts';
 
 @Component({
   selector: 'dapp-home',
@@ -6,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(): void {}
+  betTokenAddress = environment.betTokenAddress;
+  gameFactoryAddress = environment.gameFactoryAddress;
+  calculatorAddress = environment.calculatorAddress;
+
+  betTokenOwner!: string;
+  gameFactoryOwner!: string;
+  calculatorOwner!: string;
+
+  constructor(private _betToken: BetTokenService, private _gameFactory: GameFactoryService) {}
+
+  ngOnInit(): void {
+    this._betToken.owner().subscribe((owner) => {
+      this.betTokenOwner = owner;
+    });
+
+    this._gameFactory.owner().subscribe((owner) => {
+      this.gameFactoryOwner = owner;
+    });
+  }
 
   public executeSelectedChange = (event: any) => {
     console.log(event);
--- frontendAngular/src/environments/environment.ts
+++ frontendAngular/src/environments/environment.ts
@@ -17,7 +17,7 @@ export const environment = {
   // gameFactoryAddress: '0x06D0020790D42df662bB04Ab01f0208Cfbbb956F',
   // HARDHAT COM GANACHE
   betTokenAddress: '0x2612Af3A521c2df9EAF28422Ca335b04AdF3ac66',
-  CalculatorAddress: '0x26b4AFb60d6C903165150C6F0AA14F8016bE4aec',
+  calculatorAddress: '0x26b4AFb60d6C903165150C6F0AA14F8016bE4aec',
   gameFactoryAddress: '0xDb56f2e9369E0D7bD191099125a3f6C370F8ed15',
 };
 
