95233e1b0999d421070e921fc74817a91b77baca
Fabiano Nascimento <dont@disturb.com>
2022-05-05 08:57:31 -0300

    Change from Ganache to local chain Hardhat

--- frontendAngular/src/environments/environment.ts
+++ frontendAngular/src/environments/environment.ts
@@ -7,20 +7,14 @@ export const environment = {
   chainCurrencyName: 'Ether',
   //Number of blocks to do historical searches (like for GameCreated events)
   numberOfBlocksInHistoricSearches: 10000,
-  //notebook Dell
-  //betTokenAddress: '0xAec06a3112Ce416701a8C0333E502376205073fc',
-  //gameFactoryAddress: '0x8F7364cF8A2383e25818552692B7399712eeb8EF',
-  //notebook Vaio - Ganache v7
-  // betTokenAddress: '0x02a0bE3A9f3B9680361462Fa3275327a16ecA2f4',
-  // gameFactoryAddress: '0x754Cf0D53248D078CdF734e3747eefDB1Aae09e0',
-  // CalculatorAddress: '0xF3510694bbbBaB4FB4C412FDe127EAF2eAC4F37f',
-  //GANACHE 2.5.4
-  // betTokenAddress: '0x85e855b22F01BdD33eE194490c7eB16b7EdaC019',
-  // gameFactoryAddress: '0x06D0020790D42df662bB04Ab01f0208Cfbbb956F',
-  // HARDHAT COM GANACHE
-  betTokenAddress: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24',
-  calculatorAddress: '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
-  gameFactoryAddress: '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
+  //HARDHAT NODE
+  betTokenAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+  calculatorAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+  gameFactoryAddress: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+  // GANACHE
+  // betTokenAddress: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24',
+  // calculatorAddress: '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
+  // gameFactoryAddress: '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
 };
 
 /*
