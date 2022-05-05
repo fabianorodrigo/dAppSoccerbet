b365c136fd9b4b68dcd4fb70d7b44b470bfcb370
Fabiano Nascimento <dont@disturb.com>
2022-05-05 10:19:12 -0300

    Volta pro Ganache

--- frontendAngular/src/environments/environment.ts
+++ frontendAngular/src/environments/environment.ts
@@ -8,13 +8,13 @@ export const environment = {
   //Number of blocks to do historical searches (like for GameCreated events)
   numberOfBlocksInHistoricSearches: 10000,
   //HARDHAT NODE
-  betTokenAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-  calculatorAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-  gameFactoryAddress: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+  // betTokenAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+  // calculatorAddress: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+  // gameFactoryAddress: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
   // GANACHE
-  // betTokenAddress: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24',
-  // calculatorAddress: '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
-  // gameFactoryAddress: '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
+  betTokenAddress: '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24',
+  calculatorAddress: '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B',
+  gameFactoryAddress: '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7',
 };
 
 /*
